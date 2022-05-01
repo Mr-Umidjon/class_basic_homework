695431c6bc154a04baba4dbc6a93a02f050c8c85
Umidjon <mrumidjon2020@gmail.com>
2022-05-01 16:29:21 +0500

    create an objects named p1 and  p2 whose names are Shavkat and Anvar

--- create_some_objects.py
+++ create_some_objects.py
@@ -1,4 +1,7 @@
 from create_one_attribute import Person
 
-#create an object named "p1" whose name is "Anvar"
-#create an object named "p2" whose name is "Shavkat"
+# create an object named "p1" whose name is "Anvar"
+# g
+
+p1 = Person("Anvar")
+p2 = Person("Shavkat")
