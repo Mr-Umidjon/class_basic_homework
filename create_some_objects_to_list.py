982d3c096f566b608a6ef9590c9bba40745014ab
Umidjon <mrumidjon2020@gmail.com>
2022-05-01 16:34:39 +0500

    Add these objects to the persons named list

--- create_some_objects_to_list.py
+++ create_some_objects_to_list.py
@@ -1,7 +1,16 @@
 from create_one_attribute import Person
 
-#Create an object named "p1" whose name is "Anvar"
-#Create an object named "p2" whose name is "Shavkat"
-#Create an object named "p3" whose name is "Jasur"
+# Create an object named "p1" whose name is "Anvar"
+# Create an object named "p2" whose name is "Shavkat"
+# Create an object named "p3" whose name is "Jasur"
 
-#Add these objects to the "persons" named list
+# Add these objects to the "persons" named list
+
+persons = []
+
+p1 = Person("Anvar")
+persons.append(p1)
+p2 = Person("Shavkat")
+persons.append(p2)
+p3 = Person("Jasur")
+persons.append(p3)
